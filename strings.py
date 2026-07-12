5bf6e605e2f4caf8536d2d82089ce6aebb2a33f2
Vincento26 <discusswithvincent@gmail.com>
2026-07-12 11:02:34 +0100

    Work on Strings

---
+++ strings.py
@@ -0,0 +1,16 @@
+name = 'ada lovelace'
+print(name.title())
+print(name.upper())
+print(name.lower())
+first_name = 'ada'
+second_name = 'lovelace'
+full_name = first_name + ' ' + second_name
+print(full_name)
+print("Hello, " + full_name.title() + "!")
+print('\tpython')
+print("Language:\n\tPython\n\tC\n\tJavaScript")
+favourite_language = 'python '
+favourite_language = favourite_language.rstrip()
+print(favourite_language)
+message = "One of Python's strengths is its diverse community."
+print(message)
